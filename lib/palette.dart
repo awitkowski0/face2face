965908f4f0f1c474c5a912b242073afe245dd24b
awitkowski0 <alex.j.witkowski@gmail.com>
2022-10-10 18:25:38 -0400

    * Added a bottom Nav Bar * Added a top button * Added a camera module * Added an "authentication" state, not used yet

---
+++ lib/palette.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  Palette._();
+
+  static const MaterialColor primary = MaterialColor(
+    0xF89F5B,
+    <int, Color>{
+      50: Color(0xFFFEEAE6),
+      100: Color(0xFFFEDBD0),
+      200: Color(0xFFFDBAAE),
+      300: Color(0xFFFCA48B),
+      400: Color(0xFFFCA48B),
+      500: Color(0xFFF89F5B),
+      600: Color(0xFFF89F5B),
+      700: Color(0xFFF89F5B),
+      800: Color(0xFFF89F5B),
+      900: Color(0xFFF89F5B),
+    },
+  );
+}
