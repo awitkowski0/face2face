aa23cf1686e7d5911cd13555828b1a61473a6acb
muirmb <mbm79@pitt.edu>
2022-10-18 01:04:40 -0400

    Played around with chat screen more

--- lib/chat.dart
+++ lib/chat.dart
@@ -14,37 +14,57 @@ class _ChatState extends State<ChatPage> {
     // This method is specific to the camera
     Widget _buildChat() {
       return Container(
-        height: 56.0, // in logical pixels
+        width: MediaQuery.of(context).size.width,
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(color: Color(0xFFFDBAAE)),
+        decoration: BoxDecoration(color: Color(0xFFFDCABE)),
         child: Column(
           children: [
+            Padding(padding: EdgeInsets.only(top: 60)),
             Container(
               height: 50,
               decoration: BoxDecoration(
                 border: Border.all(
-                  width:10,
-                  color: Color(0xFF000000)
+                  width:2,
+                  color: Color(0xFF606060)
                 ),
-                color: Color(0xEFED5AAE)
+                color: Color(0xEFEF9A8E)
               ),
-              width: 200,
-              child: Text(
-                "Hello"
+              width: MediaQuery.of(context).size.width - 20,
+              child: Row(
+                children: [
+                  Padding(padding: EdgeInsets.only(left:5)),
+                  Image.asset('assets/images/face2face.png'),
+                  Padding(padding: EdgeInsets.only(left:5)),
+                  Text("Name", style: TextStyle(fontSize: 20, fontFamily: "Roboto")),
+                  Container(
+                    width: 250,
+                    child: Text("Message", style: TextStyle(fontSize: 20, fontFamily: "Roboto"), textAlign: TextAlign.right,)
+                  )
+                ]
               )
             ),
+            Padding(padding: EdgeInsets.only(top: 10)),
             Container(
                 height: 50,
                 decoration: BoxDecoration(
                     border: Border.all(
-                        width:10,
-                        color: Color(0xFF000000)
+                        width:2,
+                        color: Color(0xFF606060)
                     ),
-                    color: Color(0xEFED5AAE)
+                    color: Color(0xEFEF9A8E)
                 ),
-                width: 200,
-                child: Text(
-                    "Hello"
+                width: MediaQuery.of(context).size.width - 20,
+                child: Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left:5)),
+                      Image.asset('assets/images/face2face.png'),
+                      Padding(padding: EdgeInsets.only(left:5)),
+                      Text("Name", style: TextStyle(fontSize: 20, fontFamily: "Roboto")),
+                      Container(
+                          width: 250,
+                          child: Text("Message", style: TextStyle(fontSize: 20, fontFamily: "Roboto"), textAlign: TextAlign.right,)
+                      )
+                    ]
                 )
             ),
           ],
