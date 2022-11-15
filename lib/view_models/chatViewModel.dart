80ada5ba84099f5184a99234b028e357ce08a304
muirmb <mbm79@pitt.edu>
2022-11-15 11:43:23 -0500

    update

--- lib/view_models/chatViewModel.dart
+++ lib/view_models/chatViewModel.dart
@@ -8,13 +8,32 @@ final List<chat> initialData = [
   chat('I like Fishin', 'Hailey', 'Alex', true),
   chat('OMG me too! I like fishin and huntin! JACOB BIEHL', 'Alex', 'Hailey', false),
 ];
+final List<String> allNames = ["Alex"];
 
 class ChatViewModel with ChangeNotifier {
   // All chats (that will be displayed on the Home screen)
   final List<chat> _chats = initialData;
+  final List<String> _names = allNames;
+
+  List<String> get names => _names;
+
+  List<List<chat>> _byName = [];
 
   // Retrieve all chats
   List<chat> get chats => _chats;
+
+  List<List<chat>> get byName {
+    if(_byName.length == 0) {
+      for (var name in names) {
+        _byName.add(getNamedchats(name));
+      }
+    }
+    return _byName;
+  }
+  
+  List<chat> getNamedchats(String name){
+    return _chats.where((element) => element.senderName == name || element.receiverName == name).toList();
+  }
 
   // Send a chat
   void sendChat(chat message) {
